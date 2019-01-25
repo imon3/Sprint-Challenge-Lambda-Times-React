210e5b050da580c15a72b4ccd732b68f5630fe2a
Imon Ovbude <imonovbude@gmail.com>
2019-01-25 09:16:00 -0800

    mapped over tabs and cards to display on screen, added prop types to Card.js

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -5,8 +5,12 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
+        {props.tabs.map((tab, index) => {
+          return <Tab tab={tab} key={index} selectTabHandler={props.selectTabHandler} selectedTab={props.selectedTab} />
+        })}
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+
       </div>
     </div>
   );
